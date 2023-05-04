0e642c40137a005e4caac066a08081c2a2ef58d3
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-05-04 23:09:25 +0100

    update nav mobile animation

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,14 +1,17 @@
 <script>
+	import { fade, fly } from 'svelte/transition';
 	import gfc from '$lib/images/gfc.svg';
 
 	const navigation = [
-		{ name: 'Work', route: 'work' },
-		{ name: 'About', route: 'about' },
-		{ name: 'Careers', route: 'careers' },
-		{ name: 'Contact', route: 'contact' }
+		{ name: 'Work', path: 'work' },
+		{ name: 'About', path: 'about' },
+		{ name: 'Careers', path: 'careers' },
+		{ name: 'Contact', path: 'contact' }
 	];
 
 	let mobileMenuOpen = false;
+	const openMobileMenu = () => (mobileMenuOpen = true);
+	const closeMobileMenu = () => (mobileMenuOpen = false);
 </script>
 
 <header class="bg-white">
@@ -21,6 +24,7 @@
 			<button
 				type="button"
 				class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+				on:click={openMobileMenu}
 			>
 				<span class="sr-only">Open main menu</span>
 				<svg
@@ -40,51 +44,59 @@
 			</button>
 		</div>
 		<div class="hidden sm:flex sm:gap-x-12">
-			{#each navigation as { name, route }}
-				<a href="/{route}" class="text-sm font-semibold leading-6 text-gray-900">{name}</a>
+			{#each navigation as { name, path }}
+				<a href="/{path}" class="text-sm font-semibold leading-6 text-gray-900">{name}</a>
 			{/each}
 		</div>
 	</nav>
 	<!-- Mobile menu, show/hide based on menu open state. -->
-	<div class="sm:hidden" role="dialog" aria-modal="true">
-		<!-- Background backdrop, show/hide based on slide-over state. -->
-		<div class="fixed inset-0 z-10" />
-		<div
-			class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-		>
-			<div class="flex items-center justify-between">
-				<a href="/" class="-m-1.5 p-1.5">
-					<span class="sr-only">Good Fortune Collective</span>
-					<img class="h-8 w-auto" src={gfc} alt="" />
-				</a>
-				<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-					<span class="sr-only">Close menu</span>
-					<svg
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
+	{#if mobileMenuOpen}
+		<div class="sm:hidden" role="dialog" aria-modal="true" in:fade={{}} out:fade={{ delay: 400 }}>
+			<!-- Background backdrop, show/hide based on slide-over state. -->
+			<div class="fixed inset-0 z-10" />
+			<div
+				class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+			>
+				<div class="flex items-center justify-between">
+					<a href="/" class="-m-1.5 p-1.5" on:click={closeMobileMenu}>
+						<span class="sr-only">Good Fortune Collective</span>
+						<img class="h-8 w-auto" src={gfc} alt="" />
+					</a>
+					<button
+						type="button"
+						class="-m-2.5 rounded-md p-2.5 text-gray-700"
+						on:click={closeMobileMenu}
 					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button>
-			</div>
-			<div class="mt-6 flow-root">
-				<div class="-my-6 divide-y divide-gray-500/10">
-					<div class="space-y-2 py-6">
-						{#each navigation as { name, route }}<a
-								href="/{route}"
-								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-								>{name}</a
-							>
-						{/each}
+						<span class="sr-only">Close menu</span>
+						<svg
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</button>
+				</div>
+				<div class="mt-6 flow-root">
+					<div class="-my-6 divide-y divide-gray-500/10">
+						<div class="space-y-2 py-6">
+							{#each navigation as { name, path }, i}<a
+									href="/{path}"
+									class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+									in:fly={{ x: -48, duration: 500, delay: 300 + 50 * i }}
+									out:fly={{ x: -48, duration: 500, delay: 50 * i }}
+									on:click={closeMobileMenu}>{name}</a
+								>
+							{/each}
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	{/if}
 </header>
 
 <style>
