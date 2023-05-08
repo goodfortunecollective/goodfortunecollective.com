3d4785de5915f207061ae8b3446aae874ae23262
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-05-08 16:15:58 +0100

    Add https localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+import mkcert from 'vite-plugin-mkcert'
+
 export default defineConfig({
-	plugins: [sveltekit()],
+	server: { https: true },
+	plugins: [mkcert(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
