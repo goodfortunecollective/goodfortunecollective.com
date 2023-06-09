2f62d6978ff729511cf6f4d0a1b628aa5f0c96e5
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-06-09 14:47:20 +0100

    Updates

--- _app/immutable/chunks/paths.c66e024b.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1bgxbsp)==null?void 0:s.base)??"/goodfortunecollective.com";var e;const o=((e=globalThis.__sveltekit_1bgxbsp)==null?void 0:e.assets)??t;export{o as a,t as b};
