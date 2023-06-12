6a210678dc2a3d914eab555af39a4a67667319e7
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-06-13 00:33:23 +0100

    add section homepage example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import { delay_anim_page } from '$lib/stores';
 	// import VideoWithPreview from '$lib/components/VideoWithPreview.svelte';
 
+	import { Flex } from '$lib/components/layout';
 	import { Heading } from '$lib/components/typography';
 
 	export let data;
@@ -189,11 +190,25 @@
 </section>
 <div class="section-wrapper" />
 
+<section class="pb-32">
+	<div class="mx-auto max-w-6xl">
+		<Flex class="pt-16 items-end">
+			<Heading as="h2" size="h1" class="w-1/2">Headline Statement</Heading>
+			<p class="text-xl flex-1">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac feugiat nisl, a
+				cursus orci. Pellentesque sollicitudin feugiat ipsum, eget venenatis urna mollis ac.
+			</p>
+		</Flex>
+	</div>
+</section>
+
+<!--
 <section class="h-screen w-screen">
 	{#if data.story}
 		<StoryblokComponent blok={data.story.content} />
 	{/if}
 </section>
+-->
 
 <style>
 	.section-wrapper {
