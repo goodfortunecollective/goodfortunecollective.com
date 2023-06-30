97197e9fc3100fc771f977caa5490a77e30c9033
Maxime Bérard <contact@maximeberard.com>
2023-06-30 14:19:09 +0200

    Project : use "name" instead of "heading" block

--- src/routes/work/[slug]/+page.svelte
+++ src/routes/work/[slug]/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
 	import { Heading } from '$lib/components/typography';
-	import { useStoryblokBridge, StoryblokComponent } from '@storyblok/svelte';
+	import { useStoryblokBridge, StoryblokComponent, renderRichText } from '@storyblok/svelte';
 
 	import { base } from '$app/paths';
 
 	export let data;
+	$: solutions = renderRichText(data.story.content.solutions);
+	$: roles = renderRichText(data.story.content.roles);
+	$: deliverables = renderRichText(data.story.content.deliverables);
 
 	onMount(() => {
 		if (data.story) {
@@ -22,6 +25,42 @@
 </svelte:head>
 
 <section class="pt-[var(--header-height)] pb-32">
+	<div class="flex flex-row project-header">
+		<div class="flex flex-col project-header-text md:w-3/4">
+			<div class="flex flex-row gap-4 project-header-infos">
+				{#if data.story.content.client}
+					<div class="project-header-col">
+						<h5 class="mb-2 text-sm font-bold uppercase">Client</h5>
+						{data.story.content.client}
+					</div>
+				{/if}
+				{#if data.story.content.solutions}
+					<div class="project-header-col">
+						<h5 class="mb-2 text-sm font-bold uppercase">Solutions</h5>
+						{@html solutions}
+					</div>
+				{/if}
+				{#if data.story.content.roles}
+					<div class="project-header-col">
+						<h5 class="mb-2 text-sm font-bold uppercase">Roles</h5>
+						{@html roles}
+					</div>
+				{/if}
+				{#if data.story.content.deliverables}
+					<div class="project-header-col">
+						<h5 class="mb-2 text-sm font-bold uppercase">Deliverables</h5>
+						{@html deliverables}
+					</div>
+				{/if}
+			</div>
+			<Heading size="h1">{data.story.name}</Heading>
+		</div>
+		<figure class="project-thumbnail md:w-1/4">
+			{#if data.story.content.thumbnail}
+				<img src={data.story.content.thumbnail.filename} alt={data.story.content.title} />
+			{/if}
+		</figure>
+	</div>
 	<div class="max-w-6xl mx-auto">
 		<a href="{base}/work/"> &#10229; </a>
 		{#if data.story}
@@ -33,5 +72,14 @@
 	</div>
 </section>
 
-<style>
+<style lang="scss">
+	// @import '../../vars.scss';
+	.project-header-col {
+		h5 {
+			letter-spacing: 0.02em;
+		}
+		p {
+			margin: 0;
+		}
+	}
 </style>
