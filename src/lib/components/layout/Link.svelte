958921ac47019efd061ab8cbdb6c52a211a9abe2
Maxime Bérard <contact@maximeberard.com>
2023-07-04 15:21:50 +0200

    Link : arrow styles & hover

--- src/lib/components/layout/Link.svelte
+++ src/lib/components/layout/Link.svelte
@@ -9,12 +9,110 @@
 	href={href.url}
 	target={isExternal ? '_blank' : undefined}
 	rel={isExternal ? 'noopener' : undefined}
-	class="flex flex-row items-stretch h-10 text-xs font-bold tracking-widest uppercase"
+	class="flex flex-row items-stretch h-10 text-xs font-bold tracking-widest uppercase link"
 >
 	<span class="flex items-center pr-4">{label}</span>
-	<span class="relative flex text-4xl"
-		><span class="absolute z-10 font-normal top-[-6px]">&#10230;</span><span
-			class="absolute top-0 block w-10 h-10 bg-gray-300 rounded-full left-3"
-		/></span
-	>
-</a>
+	<span class="relative flex items-center justify-center ml-[50px] text-4xl">
+		<span class="arrow arrow-default" /><span class="flex items-center justify-center link-circle">
+			<span class="arrow arrow-hover" />
+		</span>
+	</span></a
+>
+
+<style lang="scss">
+	@import '../../../vars.scss';
+
+	.link {
+		&:hover {
+			.link-circle {
+				&:before {
+					transform: scale(1) translate(0, 0);
+				}
+			}
+			.arrow-default {
+				transform: scale(0, 1) translate(45px, 0);
+			}
+
+			.arrow-hover {
+				transform: translate(0, 0);
+			}
+		}
+	}
+
+	.link-circle {
+		position: absolute;
+		right: 0;
+		width: 40px;
+		height: 40px;
+		border-radius: 40px;
+		overflow: hidden;
+		background: $grayUltraLight;
+		overflow: hidden;
+		z-index: 8;
+
+		&:before {
+			position: absolute;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			left: 0;
+			border-radius: 40px;
+			transform: scale(1.2) translate(-100%, 0);
+			background: $black;
+			z-index: 9;
+			content: '';
+			transition: 0.4s transform ease-out, 0.4s opacity ease-out;
+		}
+	}
+
+	.arrow {
+		position: absolute;
+		width: 50%;
+		height: 1px;
+		transition: 0.4s transform ease-out, 0.4s opacity ease-out;
+
+		&:before,
+		&:after {
+			width: 4px;
+			height: 1px;
+			position: absolute;
+			top: 0;
+			right: 0;
+			content: '';
+		}
+
+		&:before {
+			transform: rotate(40deg);
+			transform-origin: top right;
+		}
+		&:after {
+			transform: rotate(-40deg);
+			transform-origin: bottom right;
+		}
+
+		&.arrow-default {
+			width: 29px;
+			right: 20px;
+			background: #313131;
+			z-index: 15;
+			transform-origin: 100% 50%;
+
+			&:before,
+			&:after {
+				background: $grayLight;
+			}
+		}
+
+		&.arrow-hover {
+			width: 38%;
+			background: $white;
+			transform: translate(-100%, 0);
+			z-index: 10;
+
+			&:before,
+			&:after {
+				background: $white;
+			}
+		}
+	}
+</style>
