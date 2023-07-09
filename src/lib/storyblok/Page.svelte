a6d07d3e3e15c68d70a2e7d96d227dc451c6dd1c
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-07-09 01:50:38 +0100

    Fix SEO block page

--- src/lib/storyblok/Page.svelte
+++ src/lib/storyblok/Page.svelte
@@ -3,8 +3,12 @@
 
 	export let blok: any;
 
-	let { body } = blok;
+	let { body, seo } = blok;
 </script>
+
+{#if seo?.length > 0}
+	<StoryblokComponent blok={seo[0]} />
+{/if}
 
 <div use:storyblokEditable={body}>
 	{#each body as blok}
