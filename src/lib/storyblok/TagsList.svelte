704612832b30eae06943a92dc741463e61bc6b2c
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-07-11 17:57:19 +0100

    add scroll anim tags list

--- src/lib/storyblok/TagsList.svelte
+++ src/lib/storyblok/TagsList.svelte
@@ -2,25 +2,50 @@
 	import { StoryblokComponent, storyblokEditable } from '@storyblok/svelte';
 	import { onMount } from 'svelte';
 
+	import { gsap } from '$lib/gsap';
+
 	export let blok: any;
 
 	onMount(() => {
-		console.log(blok);
+		gsap.utils.toArray<HTMLElement>('[data-gsap="tags-list"]').forEach((parent, index) => {
+			const container: HTMLElement | null = parent.querySelector('ul');
+
+			if (container) {
+				const [x, xEnd] =
+					index % 2
+						? ['100%', (container.scrollWidth - parent.offsetWidth) * -0.25]
+						: [container.scrollWidth * -1 * 0.25, 0];
+
+				gsap.fromTo(
+					container,
+					{ x },
+					{
+						x: xEnd,
+						scrollTrigger: {
+							trigger: parent,
+							scrub: 0.5
+						}
+					}
+				);
+			}
+		});
 	});
 </script>
 
 <div use:storyblokEditable={blok} class={blok.class}>
 	<div class="flex flex-col gap-8">
 		{#each Array(4) as _, index (index)}
-			<ul
-				class="whitespace-nowrap overflow-hidden border-b-2 border-b-gray-200 last-of-type:border-0"
-			>
-				{#each blok.tags as b}
-					<li class="inline-block mr-24 mb-8">
-						<span class="text-7xl uppercase"><StoryblokComponent blok={b} /></span>
-					</li>
-				{/each}
-			</ul>
+			<div data-gsap="tags-list" class="border-b-2 border-b-gray-200 last-of-type:border-0">
+				<ul class="whitespace-nowrap">
+					{#each blok.tags as b}
+						<li class="inline-block mr-24 mb-8">
+							<span class="text-7xl uppercase">
+								<StoryblokComponent blok={b} />
+							</span>
+						</li>
+					{/each}
+				</ul>
+			</div>
 		{/each}
 	</div>
 </div>
