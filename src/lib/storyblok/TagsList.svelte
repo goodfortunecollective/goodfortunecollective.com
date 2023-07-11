802b26b93ae8813c53d430adfd595d5f8700eeaf
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-07-11 16:28:16 +0100

    add tags-list blok

---
+++ src/lib/storyblok/TagsList.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { StoryblokComponent, storyblokEditable } from '@storyblok/svelte';
+	import { onMount } from 'svelte';
+
+	export let blok: any;
+
+	onMount(() => {
+		console.log(blok);
+	});
+</script>
+
+<div use:storyblokEditable={blok} class={blok.class}>
+	<div class="flex flex-col gap-8">
+		{#each Array(4) as _, index (index)}
+			<ul
+				class="whitespace-nowrap overflow-hidden border-b-2 border-b-gray-200 last-of-type:border-0"
+			>
+				{#each blok.tags as b}
+					<li class="inline-block mr-24 mb-8">
+						<span class="text-7xl uppercase"><StoryblokComponent blok={b} /></span>
+					</li>
+				{/each}
+			</ul>
+		{/each}
+	</div>
+</div>
