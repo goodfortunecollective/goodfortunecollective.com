f6163b4cdcdb43a8bdd2655e7200746dd6be4034
Maxime Bérard <contact@maximeberard.com>
2023-07-11 14:09:12 +0200

    Images carousel styles

--- src/lib/storyblok/ImagesCarousel.svelte
+++ src/lib/storyblok/ImagesCarousel.svelte
@@ -3,43 +3,39 @@
 
 	export let blok: any;
 
-	export let imagesLength = blok.images.length;
-	export let blockClasses = 'flex py-12 images-block'; // flex-col
-	export let imageClass = 'image-block';
-	export let imagesCols = ['', '', ''];
-	export let imagesClasses = ['', '', ''];
+	export let blockClasses = 'flex py-12 images-block  overflow-hidden'; // flex-col
+	export let wrapperClasses = 'flex w-[4000px] md:flex-row flex-nowrap md:gap-4'; // flex-col
+	export let imageClass = 'flex flex-col image-block';
+	// export let imagesCols = [];
+	export let imagesClasses = [];
 
-	if (imagesLength == 1) {
-		blockClasses += ' w-full justify-center';
-	} else if (imagesLength >= 3) {
-		blockClasses += ' max-w-6xl mx-auto md:flex-row md:gap-4';
-		imageClass = 'basis-full';
-	}
-
-	for (let i = 0; i < imagesCols.length; i++) {
-		imagesClasses[i] = imageClass + ' ' + imagesCols[i];
+	for (let i = 0; i < blok.images.length; i++) {
+		if (i == 0) imagesClasses[i] = imageClass + ' md:ml-24';
+		else imagesClasses[i] = imageClass + ' ';
 	}
 </script>
 
 <div use:storyblokEditable={blok} {...$$restProps} class={blok.class}>
 	<div class={blockClasses}>
-		{#each blok.images as item, i}
-			<figure class={imagesClasses[i % 3]}>
-				<img
-					class="image-block-img"
-					src={item.filename}
-					alt={item.name ? item.name : 'Image ' + i}
-				/>
-				{#if item.name && blok.show_title}
-					<figcaption class="mt-4 font-bold tracking-widest uppercase image-block-caption">
-						<span class="caption-number"
-							>{#if i <= 10}0{/if}{i + 1}</span
-						>
-						<span class="opacity-50 caption-text">{item.name}</span>
-					</figcaption>
-				{/if}
-			</figure>
-		{/each}
+		<div class={wrapperClasses}>
+			{#each blok.images as item, i}
+				<figure class={imagesClasses[i]}>
+					<img
+						class="image-block-img"
+						src={item.filename}
+						alt={item.name ? item.name : 'Image ' + i}
+					/>
+					{#if item.name && blok.show_title}
+						<figcaption class="mt-4 font-bold tracking-widest uppercase image-block-caption">
+							<span class="caption-number"
+								>{#if i <= 10}0{/if}{i + 1}</span
+							>
+							<span class="opacity-50 caption-text">{item.name}</span>
+						</figcaption>
+					{/if}
+				</figure>
+			{/each}
+		</div>
 	</div>
 </div>
 
@@ -57,11 +53,10 @@
 
 	.image-block {
 		position: relative;
+		width: 50vw;
 
 		@media (min-width: $media-md) {
-			&:nth-child(2) {
-				margin-left: -20%;
-			}
+			width: 30vw;
 		}
 	}
 
