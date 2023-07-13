c826b7cc701992cb1c81bb59edbe1b737c4a41d2
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-07-13 01:22:54 +0100

    headling list - fix column layout

--- src/lib/storyblok/HeadingList.svelte
+++ src/lib/storyblok/HeadingList.svelte
@@ -7,31 +7,48 @@
 
 	export let blok: any;
 
-	const variants = cva('relative max-w-6xl mx-auto px-6 xl:px-0 md:flex justify-between', {
+	const variants = cva('relative', {
 		variants: {
 			layoutDirection: {
 				left: 'flex-row',
 				right: 'flex-row-reverse'
+			},
+			columnIndex: {
+				'0': 'md:w-5/12',
+				'1': '',
+				'2': 'md:w-4/12',
+				'3': 'md:w-1/12'
 			}
 		},
 		defaultVariants: {
 			layoutDirection: 'left'
 		}
 	});
+
+	const getColumnIndex = (index: number) => (index % 4).toString() as '0' | '1' | '2' | '3';
 </script>
 
 <div
 	use:storyblokEditable={blok}
 	{...$$restProps}
-	class={cls(variants({ layoutDirection: blok.layoutDirection }), blok.class)}
+	class={cls(
+		'max-w-6xl mx-auto px-6 xl:px-0 md:flex justify-between',
+		variants({ layoutDirection: blok.layoutDirection }),
+		blok.class
+	)}
 >
 	<div class="md:w-4/12 absolute">
 		<Heading as="h3" size="h2">{blok.heading}</Heading>
 	</div>
 
-	<dl class="flex flex-col md:w-7/12 gap-16 mt-16">
-		{#each blok.list as item, index}
-			<StoryblokComponent blok={item} {index} />
+	<dl class="flex flex-col gap-24 mt-16">
+		{#each blok.list as b, index}
+			<div class={cls('flex justify-start', variants({ layoutDirection: blok.layoutDirection }))}>
+				<div class={cls(variants({ columnIndex: getColumnIndex(index) }))} />
+				<div class="md:w-7/12">
+					<StoryblokComponent blok={b} {index} />
+				</div>
+			</div>
 		{/each}
 	</dl>
 </div>
