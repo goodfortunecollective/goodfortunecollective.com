d10cebc9e76dbc53f12fa22bf60f5620d85d51e4
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-08-06 00:34:44 +0100

    add footer dynamic theme

--- src/lib/storyblok/Footer.svelte
+++ src/lib/storyblok/Footer.svelte
@@ -2,11 +2,17 @@
 	import { cva } from 'class-variance-authority';
 	import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte';
 
+	import { afterNavigate } from '$app/navigation';
+	import { page } from '$app/stores';
 	import { cls } from '$lib/styles';
 
 	export let blok: any;
 
-	export let theme: 'dark' | 'light' = 'light';
+	const getTheme = (pathname: string) => {
+		return pathname === '/' ? 'dark' : 'light';
+	};
+
+	let theme: 'light' | 'dark' = getTheme($page.url.pathname);
 
 	const variants = cva('', {
 		variants: {
@@ -19,13 +25,17 @@
 			theme: 'light'
 		}
 	});
+
+	afterNavigate(async ({ to }) => {
+		theme = getTheme(to?.url.pathname!);
+	});
 </script>
 
 <footer
 	use:storyblokEditable={blok}
 	{...$$restProps}
 	id="footer"
-	class={cls('pt-1 -translate-y-1', variants({ theme: theme }))}
+	class={cls('pt-1 -translate-y-1', variants({ theme }))}
 >
 	<div class="w-full py-6 mx-auto">
 		<div class="grid grid-cols-12 py-2">
@@ -44,32 +54,7 @@
 		<div class="grid grid-cols-12 py-2">
 			<div class="col-span-4 col-start-2 pt-2 pb-8 text-sm text-gray-400 md:pb-0">
 				<StoryblokComponent blok={blok.social_links[0]} />
-				<!--<SocialLinks />-->
 			</div>
-			<!-- <div class="flex justify-center pb-8 space-x-6 text-sm md:pb-0">
-				<a
-					href="https://www.facebook.com/GoodFortuneCollective/"
-					class="text-gray-400 hover:text-gray-500"
-				>
-					<span class="sr-only">Facebook</span>
-					<span class="">Fb</span>
-				</a>
-				<a
-					href="https://www.instagram.com/goodfortunecollective/"
-					class="text-gray-400 hover:text-gray-500"
-				>
-					<span class="sr-only">Instagram</span>
-					<span class="">Ig</span>
-				</a>
-				<a href="https://vimeo.com/goodfortunecollective" class="text-gray-400 hover:text-gray-500">
-					<span class="sr-only">Vimeo</span>
-					<span class="">Vi</span>
-				</a>
-				<a href="https://www.behance.net/" class="text-gray-400 hover:text-gray-500">
-					<span class="sr-only">Behance</span>
-					<span class="">Be</span>
-				</a>
-			</div> -->
 			<div class="col-span-4 col-start-8">
 				<p class="text-xs 3xl:text-sm leading-5 tracking-wider text-gray-500 uppercase">
 					&copy; {new Date().getFullYear().toString()} Good Fortune Collective. All rights reserved.
