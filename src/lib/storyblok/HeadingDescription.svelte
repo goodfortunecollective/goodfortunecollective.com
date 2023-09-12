ce44f50c08478693ae4dbf595df1568ea793a87c
Maxime Bérard <contact@maximeberard.com>
2023-09-12 12:36:47 +0200

    Changed heading h6 size on md screens

--- src/lib/storyblok/HeadingDescription.svelte
+++ src/lib/storyblok/HeadingDescription.svelte
@@ -7,14 +7,14 @@
 </script>
 
 <div use:storyblokEditable={blok} {...$$restProps} class={blok.class}>
-	<div class="grid grid-cols-12 pt-24 md:pt-32 gap-8 md:gap-0">
+	<div class="grid grid-cols-12 gap-8 pt-24 md:pt-32 md:gap-0">
 		<Heading
 			as="h2"
 			size="h6"
-			class="col-start-2 md:col-start-3 col-span-10  md:col-span-3 font-bold tracking-widest uppercase  w-full lg:w-1/2 title"
+			class="w-full col-span-10 col-start-2 font-bold tracking-widest uppercase md:col-start-3 md:col-span-3 xl:w-[75%] title break-keep"
 			>{blok.heading}</Heading
 		>
-		<div class="col-start-2 md:col-start-7 col-span-10 md:col-span-5 text-lg">
+		<div class="col-span-10 col-start-2 text-lg md:col-start-7 md:col-span-5">
 			<div class="flex flex-col gap-{blok.gap}">
 				{#each blok.content as b}
 					<StoryblokComponent blok={b} />
