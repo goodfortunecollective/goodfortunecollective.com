316c12c0f41761dab2cede0987033b35844f7216
Maxime Bérard <contact@maximeberard.com>
2023-09-13 14:57:48 +0200

    Footer : back to top btn

--- src/lib/storyblok/Footer.svelte
+++ src/lib/storyblok/Footer.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { cva } from 'class-variance-authority';
 	import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte';
 
 	import { afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { cls } from '$lib/styles';
+	import gsap, { ScrollToPlugin } from '$lib/gsap';
 
 	export let blok: any;
+
+	let backToTopBtn: HTMLElement;
 
 	const getTheme = (pathname: string) => {
 		return pathname === '/' ? 'dark' : 'light';
@@ -29,6 +33,15 @@
 	afterNavigate(async ({ to }) => {
 		theme = getTheme(to?.url.pathname!);
 	});
+
+	const backToTop = (event: any) => {
+		gsap.to(window, { scrollTo: { y: 0 }, duration: 0.8 });
+		event.preventDefault();
+	};
+
+	onMount(async () => {
+		backToTopBtn.addEventListener('click', backToTop);
+	});
 </script>
 
 <footer
@@ -37,21 +50,21 @@
 	id="footer"
 	class={cls('pt-1 -translate-y-1', variants({ theme }))}
 >
-	<div class="w-full py-6">
+	<div class="relative w-full py-6">
 		<div class="grid grid-cols-12 py-2">
 			<div
-				class="col-span-10 md:col-span-5 lg:col-span-4 col-start-2 md:col-start-2 lg:col-start-2 pt-8 pb-2 footer-col"
+				class="col-span-10 col-start-2 pt-8 pb-2 md:col-span-5 lg:col-span-4 md:col-start-2 lg:col-start-2 footer-col"
 			>
 				<p
-					class="flex flex-col md:flex-row gap-4 text-xs 3xl:text-sm leading-5 tracking-wider text-gray-500 uppercase"
+					class="flex flex-col gap-4 text-xs leading-5 tracking-wider text-gray-500 uppercase md:flex-row 3xl:text-sm"
 				>
 					<strong>Let’s work together</strong>
 					<a href={`mailto:${blok.email}`}>{blok.email}</a>
 				</p>
 			</div>
-			<div class="col-span-10 md:col-span-4 col-start-2 md:col-start-8 pt-8 pb-2 footer-col">
+			<div class="col-span-10 col-start-2 pt-8 pb-2 md:col-span-4 md:col-start-8 footer-col">
 				<p
-					class="flex flex-col md:flex-row gap-4 text-xs 3xl:text-sm leading-5 tracking-wider text-gray-500 uppercase"
+					class="flex flex-col gap-4 text-xs leading-5 tracking-wider text-gray-500 uppercase md:flex-row 3xl:text-sm"
 				>
 					<strong>Find us in</strong><span>Vancouver, Canada</span>
 				</p>
@@ -59,19 +72,26 @@
 		</div>
 		<div class="grid grid-cols-12 py-2">
 			<div
-				class="col-span-10 md:col-span-4 col-start-2 md:col-start-2 pt-2 pb-4 md:pb-0 text-sm text-gray-400"
+				class="col-span-10 col-start-2 pt-2 pb-4 text-sm text-gray-400 md:col-span-4 md:col-start-2 md:pb-0"
 			>
 				<StoryblokComponent blok={blok.social_links[0]} />
 			</div>
 			<div
-				class="col-span-10 md:col-span-4 lg:col-span-4 col-start-2 md:col-start-8 lg:col-start-8"
+				class="col-span-10 col-start-2 md:col-span-4 lg:col-span-4 md:col-start-8 lg:col-start-8"
 			>
-				<p class="text-xs 3xl:text-sm leading-5 tracking-wider text-gray-500 uppercase">
+				<p class="text-xs leading-5 tracking-wider text-gray-500 uppercase 3xl:text-sm">
 					&copy; {new Date().getFullYear().toString()} Good Fortune Collective. All rights reserved.
 				</p>
 			</div>
 		</div>
 	</div>
+	<a
+		href="#"
+		bind:this={backToTopBtn}
+		class="absolute right-[20px] md:right-[60px] top-[-90px] back-to-top-btn w-[20px] h-[66px]"
+	>
+		<div class="arrow" />
+	</a>
 </footer>
 
 <style lang="scss">
@@ -130,6 +150,62 @@
 						transform: translate(0, 0);
 					}
 				}
+			}
+		}
+	}
+
+	.back-to-top-btn {
+		opacity: 0.4;
+		transition: 0.5s opacity;
+
+		&:hover {
+			opacity: 0.6;
+		}
+
+		&:before {
+			position: absolute;
+			top: 0;
+			left: 50%;
+			width: 1px;
+			bottom: 0;
+			content: '';
+			background: white;
+		}
+
+		&:after {
+			position: absolute;
+			width: 1px;
+			left: 2px;
+			height: 100%;
+			// background: white;
+			content: '';
+		}
+
+		.arrow {
+			position: absolute;
+			top: 0;
+			left: 50%;
+			transform: translate(-50%, 0) rotate(-90deg);
+
+			&:before,
+			&:after {
+				width: 4px;
+				height: 1px;
+				position: absolute;
+				top: 0;
+				right: 0;
+				background: white;
+				content: '';
+			}
+
+			&:before {
+				transform: rotate(40deg);
+				transform-origin: top right;
+			}
+
+			&:after {
+				transform: rotate(-40deg);
+				transform-origin: bottom right;
 			}
 		}
 	}
