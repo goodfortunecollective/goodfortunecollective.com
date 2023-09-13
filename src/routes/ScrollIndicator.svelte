dabdc7a68dabd8b163f7cf3b1c7d067ef8d3ddc7
Maxime Bérard <contact@maximeberard.com>
2023-09-13 13:47:57 +0200

    Scroll indicator : hide on mobile

--- src/routes/ScrollIndicator.svelte
+++ src/routes/ScrollIndicator.svelte
@@ -31,7 +31,7 @@
 </script>
 
 <div
-	class="fixed bottom-[20px] right-[20px] lg:bottom-[60px] lg:right-[60px] z-40 w-[20px] h-[20px] mix-blend-difference"
+	class="fixed hidden md:block bottom-[20px] right-[20px] md:bottom-[60px] md:right-[60px] z-40 w-[20px] h-[20px] mix-blend-difference"
 	id="scroll-indicator"
 >
 	<div class="absolute w-full h-full border-2 border-white rounded-full opacity-20" />
