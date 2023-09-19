a33d00985072ba698984830498a7e4e22743d110
Maxime Bérard <contact@maximeberard.com>
2023-09-19 11:55:20 +0200

    Images list : changed styles according to new design

--- src/lib/storyblok/ImagesList.svelte
+++ src/lib/storyblok/ImagesList.svelte
@@ -3,25 +3,23 @@
 
 	export let blok: any;
 
-	export let titleClasses = ['md:col-start-5', 'md:col-start-11'];
-	export let imageClasses = ['md:col-start-1', 'md:col-start-14'];
-	export let textClasses = ['md:col-start-14', 'md:col-start-4'];
+	export let imageClasses = ['md:col-start-14', 'md:col-start-3'];
+	export let textClasses = ['md:col-start-3', 'md:col-start-14'];
 </script>
 
 <div use:storyblokEditable={blok} {...$$restProps} class={blok.class}>
 	<div class="flex flex-col list">
 		{#each blok.list as item, i}
-			<div class="flex px-8 py-16 md:px-0 md:grid md:grid-cols-24 md:gap-2 md:py-32 list-item">
-				<div class={'list-item-title-cont mb-8 flex flex-row md:col-span-8 ' + titleClasses[i % 2]}>
-					<div class="text-lg font-bold list-item-number">
-						{#if i <= 10}0{/if}{i + 1}
+			<div class="px-8 py-16 md:px-0 md:grid md:grid-cols-24 md:gap-2 md:py-32 list-item">
+				<div class={'list-item-text-cont flex-row md:col-span-9  ' + textClasses[i % 2]}>
+					<h2 class="mb-8 text-5xl lg:text-8xl list-item-title font-degular-display">
+						{item.title}
+					</h2>
+					<div class="mb-8 list-item-text md:mb-0">
+						<StoryblokComponent blok={item} />
 					</div>
-					<h2 class="text-4xl list-item-title">{item.title}</h2>
 				</div>
-				<div class={'list-item-text mb-8 md:mb-0 md:col-span-8 ' + textClasses[i % 2]}>
-					<StoryblokComponent blok={item} />
-				</div>
-				<figure class={'list-item-image md:col-span-12 ' + imageClasses[i % 2]}>
+				<figure class={'list-item-image mt-8 md:mt-0 md:col-span-9 ' + imageClasses[i % 2]}>
 					<img src={item.image.filename} alt={item.title} />
 				</figure>
 			</div>
@@ -38,45 +36,15 @@
 		}
 
 		@media (min-width: $media-md) {
-			&:nth-child(2n + 1) {
-				.list-item-text {
-					order: 3;
+			&:nth-child(2n + 0) {
+				.list-item-text-cont {
+					order: 2;
 				}
 
 				.list-item-image {
-					order: 2;
+					order: 1;
 				}
 			}
-		}
-	}
-
-	.list-item-number {
-		position: relative;
-		width: calc(2% / 10) * 100;
-		@media (min-width: $media-md) {
-			width: calc(3% / 8) * 100;
-		}
-
-		&:after {
-			position: absolute;
-			top: 14px;
-			right: 15px;
-			left: 30px;
-			height: 1px;
-			background: $white;
-			content: '';
-		}
-	}
-
-	.list-item-title {
-		width: calc(8% / 10) * 100;
-		@media (min-width: $media-md) {
-			width: calc(5% / 8) * 100;
-		}
-	}
-
-	.list-title {
-		@media (min-width: screen-sm-min) {
 		}
 	}
 </style>
