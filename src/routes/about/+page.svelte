cedc35486946df8d000516b67d786fce6795fa61
Maxime Bérard <contact@maximeberard.com>
2023-09-20 15:37:02 +0200

    About image block width changed

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -19,13 +19,13 @@
 		<StoryblokComponent blok={data.story.content} />
 	{/if}
 </section>
-<section class="grid grid-cols-12 mb-2 gap-8 lg:gap-0">
-	<div class="col-start-2 lg:col-start-2 col-span-10 lg:col-span-4">
+<section class="grid grid-cols-12 gap-8 mb-2 lg:gap-0">
+	<div class="col-span-10 col-start-2 lg:col-start-2 lg:col-span-4">
 		<div class="pt-0 text-2xl 4xl:text-3xl lg:pt-24">
 			Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 		</div>
 	</div>
-	<div class="col-start-2 lg:col-start-7 col-span-10 lg:col-span-4">
+	<div class="col-span-10 col-start-2 lg:col-start-7 lg:col-span-4">
 		<Heading as="h2" size="h1" class="text-9xl">Join Us Subheadline</Heading>
 		<div class="mt-8">
 			<Link href={base + '/careers/'} label="We're good people" class="mt-8" />
@@ -37,6 +37,9 @@
 	@import '../../vars.scss';
 
 	.about {
+		:global(.images-block) {
+			margin-top: 6rem;
+		}
 		:global(.image-block:after) {
 			position: absolute;
 			top: 0;
@@ -51,6 +54,17 @@
 		}
 
 		@media (min-width: $media-md) {
+			:global(.images-block) {
+				justify-content: flex-start;
+				margin-top: 8rem;
+			}
+			:global(.image-block) {
+				width: 83%;
+			}
+			:global(.image-block-img) {
+				width: 100%;
+				max-width: initial;
+			}
 			:global(.image-block:after) {
 				width: 180px;
 				height: 180px;
