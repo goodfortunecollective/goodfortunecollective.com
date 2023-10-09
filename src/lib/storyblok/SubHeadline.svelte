a8b5a1effd099f13d24c5d3a8688f48c526d15dd
Maxime Bérard <contact@maximeberard.com>
2023-10-09 16:09:51 +0200

    Subheadline & headling block : add "fullTitleWidth" option to allow new layout possibilities

--- src/lib/storyblok/SubHeadline.svelte
+++ src/lib/storyblok/SubHeadline.svelte
@@ -4,12 +4,18 @@
 	import { Heading } from '$lib/components';
 
 	export let blok: any;
+
+	let titleSize = 'h3';
+	if (blok.titleSize) titleSize = blok.titleSize;
 </script>
 
 <div use:storyblokEditable={blok} {...$$restProps} class={blok.class}>
-	<div class="grid grid-cols-12 pt-16 pb-8 gap-8 lg:gap-0">
-		<div class="col-start-2 col-span-10 lg:col-start-2 lg:col-span-3">
-			<Heading as="h2" size="h3">{blok.title}</Heading>
+	<div class="grid grid-cols-12 gap-8 pt-16 pb-8 lg:gap-0">
+		<div
+			class={'subheadline-title-cont col-span-10 col-start-2 lg:col-start-2 ' +
+				(blok.titleFullWidth ? 'lg:col-span-8' : 'lg:col-span-4')}
+		>
+			<Heading as="h2" size={titleSize} class="leading-extra-tight">{blok.title}</Heading>
 			{#if blok.links.length}
 				<div class="hidden mt-8 lg:block">
 					{#each blok.links as b}
@@ -18,7 +24,7 @@
 				</div>
 			{/if}
 		</div>
-		<div class="col-start-2 col-span-10 lg:col-span-5 md:col-start-6">
+		<div class="col-span-10 col-start-2 lg:col-span-5 md:col-start-6 lg:col-start-6">
 			<div class="flex flex-col gap-8 lg:pl-48">
 				{#each blok.content as b}
 					<StoryblokComponent blok={b} />
@@ -34,3 +40,11 @@
 		{/if}
 	</div>
 </div>
+
+<style lang="scss">
+	@import '../../vars.scss';
+
+	.subheadline-title-cont :global(.size-h1) {
+		line-height: 0.85;
+	}
+</style>
