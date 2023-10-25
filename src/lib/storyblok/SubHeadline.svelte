152a07802c7a808a7d861b1f5950c681d39ef52a
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-10-25 22:58:23 +0100

    add theme SubHeadline blok

--- src/lib/storyblok/SubHeadline.svelte
+++ src/lib/storyblok/SubHeadline.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
 	import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte';
+	import { cva } from 'class-variance-authority';
 
 	import { Heading } from '$lib/components';
+	import { cls } from '$lib/styles';
+	import { backgroundTheme } from '$lib/stores';
 
 	export let blok: any;
+
+	const variants = cva('', {
+		variants: {
+			theme: {
+				light: '',
+				dark: 'text-yellow-50'
+			}
+		},
+		defaultVariants: {
+			theme: 'light'
+		}
+	});
 </script>
 
-<div use:storyblokEditable={blok} {...$$restProps} class={blok.class}>
+<div
+	use:storyblokEditable={blok}
+	{...$$restProps}
+	class={cls(blok.class, variants({ theme: $backgroundTheme }))}
+>
 	<div class="grid grid-cols-12 gap-8 pb-8 pt-16 lg:gap-0">
 		<div
 			class={'subheadline-title-cont col-span-10 col-start-2 lg:col-start-2 ' +
