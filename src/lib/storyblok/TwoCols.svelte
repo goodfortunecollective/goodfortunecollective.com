fabf67da36d024466c29a19fe97e6c00641b5f53
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-10-25 18:31:41 +0100

    Update 2 cols blok

--- src/lib/storyblok/TwoCols.svelte
+++ src/lib/storyblok/TwoCols.svelte
@@ -4,15 +4,13 @@
 	import { Heading, RichtextTransition } from '$lib/components';
 
 	export let blok: any;
+
 	$: left_col = renderRichText(blok.left_col);
 	$: right_col = renderRichText(blok.right_col);
 </script>
 
 <div use:storyblokEditable={blok} {...$$restProps} class={blok.class}>
 	<div class="process-text grid grid-cols-12 pb-36 pt-32">
-		<div class="col-span-10 col-start-2 mb-24 md:col-span-8 md:col-start-2">
-			<Heading as="h2" size="h2" class="">{blok.title}</Heading>
-		</div>
 		<div class="col-span-10 col-start-2 mb-12 md:col-span-4 md:col-start-2 md:mb-0">
 			<Heading as="h4" size="h6" class="mb-4 uppercase">{blok.left_col_title}</Heading>
 			<RichtextTransition>{@html left_col}</RichtextTransition>
