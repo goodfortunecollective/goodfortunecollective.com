a991f20eb944616bee0de78392ab9fd1fa507e36
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-10-27 23:19:01 +0100

    update ImagesList blok

--- src/lib/storyblok/ImageList.svelte
+++ src/lib/storyblok/ImageList.svelte
@@ -2,9 +2,8 @@
 	import { renderRichText, storyblokEditable } from '@storyblok/svelte';
 	import { cva } from 'class-variance-authority';
 
-	import { cls } from '$lib/styles';
 	import { backgroundTheme } from '$lib/stores';
-	import { Heading } from '$lib/components';
+	import { Heading, RichtextTransition } from '$lib/components';
 
 	export let blok: any;
 
@@ -24,11 +23,15 @@
 </script>
 
 <div use:storyblokEditable={blok} {...$$restProps} class={blok.class}>
-	{blok.category}
-	<Heading size="h2" as="h2" class={headingStyle({ theme: $backgroundTheme })}>
-		{blok.title}
-	</Heading>
-	<div>
-		{@html list}
+	<div class="flex flex-col gap-8">
+		<Heading size="h5" as="h5" class="font-bold uppercase">{blok.category}</Heading>
+		<Heading size="h2" as="h2" class={headingStyle({ theme: $backgroundTheme })}>
+			{blok.title}
+		</Heading>
+		<div class="[&_p]:my-4 [&_p]:leading-8">
+			<RichtextTransition class="text-xl [&_p]:my-16 [&_p]:leading-8"
+				>{@html list}</RichtextTransition
+			>
+		</div>
 	</div>
 </div>
--- src/lib/storyblok/ImagesList.svelte
+++ src/lib/storyblok/ImagesList.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
+	import { onDestroy } from 'svelte';
 	import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte';
 	import { cva } from 'class-variance-authority';
 
+	import { ScrollTrigger } from '$lib/gsap';
 	import { cls } from '$lib/styles';
 	import { backgroundTheme } from '$lib/stores';
+	import { useTransitionReady } from '$lib/utils/useTransitionReady';
 
 	export let blok: any;
+
+	let el!: HTMLElement;
+	let pinEl!: HTMLElement;
+	let contentEl!: HTMLElement;
+
+	let scrollTrigger!: ScrollTrigger;
 
 	const textStyle = cva('flex flex-col', {
 		variants: {
@@ -18,6 +27,23 @@
 			theme: 'light'
 		}
 	});
+
+	useTransitionReady(() => {
+		scrollTrigger = ScrollTrigger.create({
+			trigger: el,
+			start: '-50px top',
+			end: () => `+=${contentEl.offsetHeight - pinEl.offsetHeight - 160}`,
+			pin: pinEl,
+			invalidateOnRefresh: true
+		});
+	});
+
+	onDestroy(() => {
+		if (scrollTrigger) {
+			scrollTrigger.kill();
+			scrollTrigger = null;
+		}
+	});
 </script>
 
 <div
@@ -25,11 +51,19 @@
 	{...$$restProps}
 	class={cls(blok.class, textStyle({ theme: $backgroundTheme }))}
 >
-	<div class="grid grid-cols-12">
-		<div class="col-span-6 col-start-3 flex flex-col">
-			{#each blok.list as b}
-				<StoryblokComponent blok={b} />
-			{/each}
+	<div class="relative grid grid-cols-12" bind:this={el}>
+		<img
+			bind:this={pinEl}
+			src={blok.asset.filename}
+			alt=""
+			class="-translate-x-1/8 absolute left-1/2 top-0 col-span-12 translate-y-1/4 -rotate-45 scale-150 transform"
+		/>
+		<div class="col-span-6 col-start-3 py-32" bind:this={contentEl}>
+			<div class=" flex flex-col gap-32">
+				{#each blok.list as b}
+					<StoryblokComponent blok={b} />
+				{/each}
+			</div>
 		</div>
 	</div>
 </div>
