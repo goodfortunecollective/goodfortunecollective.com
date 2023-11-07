baf76ff820d08c998f8e1318fbed2f9d5d103118
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-11-07 00:14:31 +0000

    add parallax anim blok

--- src/lib/storyblok/ImagesParallax.svelte
+++ src/lib/storyblok/ImagesParallax.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
 	import { StoryblokComponent, storyblokEditable } from '@storyblok/svelte';
+
 	import { cls } from '$lib/styles';
+	import { gsap } from 'gsap';
+	import { useTransitionReady } from '$lib/utils/useTransitionReady';
 
 	export let blok: any;
+	let element: HTMLElement;
+	let ctx: any = null;
+
+	useTransitionReady(
+		() => {
+			ctx = gsap.context(() => {
+				gsap.to(element, {
+					yPercent: blok.speed,
+					ease: 'none',
+					scrollTrigger: {
+						trigger: element,
+						start: 'top center',
+						scrub: true,
+						markers: true
+					}
+				});
+			}, element);
+		},
+		() => {
+			if (ctx) ctx.revert();
+		}
+	);
 </script>
 
 <div use:storyblokEditable={blok} {...$$restProps} class={cls('relative', blok.class)}>
-	<img src={blok.background.filename} alt="" class="" />
+	<img src={blok.background.filename} alt="" bind:this={element} />
 	{#each blok.content as b}
 		<StoryblokComponent blok={b} />
 	{/each}
--- src/lib/storyblok/ImagesParallaxItem.svelte
+++ src/lib/storyblok/ImagesParallaxItem.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
 	import { storyblokEditable } from '@storyblok/svelte';
+
 	import { cls } from '$lib/styles';
+	import { gsap } from 'gsap';
+	import { useTransitionReady } from '$lib/utils/useTransitionReady';
 
 	export let blok: any;
+	let element: HTMLElement;
+	let ctx: any = null;
+
+	useTransitionReady(
+		() => {
+			ctx = gsap.context(() => {
+				gsap.to(element, {
+					yPercent: blok.speed,
+					ease: 'none',
+					scrollTrigger: {
+						trigger: element,
+						start: 'top center',
+						scrub: true,
+						markers: true
+					}
+				});
+			}, element);
+		},
+		() => {
+			if (ctx) ctx.revert();
+		}
+	);
 </script>
 
-<div
-	use:storyblokEditable={blok}
-	{...$$restProps}
-	class={cls('absolute inset-0', blok.class)}
-	data-speed={blok.speed}
->
-	<img src={blok.asset.filename} alt="" />
+<div use:storyblokEditable={blok} {...$$restProps} class={cls('absolute inset-0', blok.class)}>
+	<img src={blok.asset.filename} alt="" bind:this={element} />
 </div>
