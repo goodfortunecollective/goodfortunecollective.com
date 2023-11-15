10251759766ebac5ac7e396e073de3ee73bc78fa
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-11-15 00:45:26 +0000

    update Image blok

--- src/lib/storyblok/Image.svelte
+++ src/lib/storyblok/Image.svelte
@@ -5,5 +5,7 @@
 </script>
 
 <div use:storyblokEditable={blok} {...$$restProps} class={blok.class}>
-	<img class="w-full" src={blok.asset.filename} alt={blok.asset.name ? blok.asset.name : ''} />
+	{#if blok.asset}
+		<img class="w-full" src={blok.asset.filename} alt={blok.asset.name ? blok.asset.name : ''} />
+	{/if}
 </div>
