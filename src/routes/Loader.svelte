5345be4f922b1398f73d6f0f084e0b6ff49cbf85
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-11-21 23:16:52 +0000

    scale down loader

--- src/routes/Loader.svelte
+++ src/routes/Loader.svelte
@@ -1,26 +1,22 @@
 <script lang="ts">
 	import { createEventDispatcher, onMount } from 'svelte';
 
-	import gsap from '$lib/gsap';
-	import { cls } from '$lib/styles';
-
 	// avoid to reload the loader animation each time we update the page
 	export let skip: boolean = false;
 
+	export const hideLoader = () => {
+		active = false;
+	};
+
 	const dispatch = createEventDispatcher();
-
-	let background!: HTMLElement;
 	let logo!: HTMLVideoElement;
-
 	let active: boolean = true;
-	let colorBackground: string = 'bg-white';
 
 	function ready() {
 		dispatch('complete');
 	}
 
 	const onVideoReady = () => {
-		console.log('video ready');
 		logo.addEventListener('ended', onVideoComplete, {
 			once: true
 		});
@@ -32,10 +28,6 @@
 		if (!skip) {
 			ready();
 		}
-	};
-
-	export const hideLoader = () => {
-		active = false;
 	};
 
 	onMount(async () => {
@@ -61,20 +53,16 @@
 </script>
 
 {#if active}
-	<div
-		class={cls('fixed left-0 top-0 z-40 h-screen w-screen overflow-hidden', colorBackground)}
-		id="loader"
-	>
+	<div class="fixed left-0 top-0 z-40 h-screen w-screen overflow-hidden bg-white" id="loader">
 		<div class="flex h-full w-full items-center justify-center">
-			<div bind:this={background} class="absolute h-full w-full bg-white" />
-
+			<div class="absolute h-full w-full bg-white" />
 			<video
 				bind:this={logo}
-				class="absolute"
+				class="absolute scale-75"
 				src={'/GFC-intro.mp4'}
 				autoplay={false}
 				muted
-				preload="auto"
+				preload="metadata"
 			/>
 		</div>
 	</div>
