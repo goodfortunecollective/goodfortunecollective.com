12b655e8445af48b4249bb024ff7a4e9e4f86bb1
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-12-04 01:16:24 +0000

    add video lazy loading with inview

--- src/lib/storyblok/HighlightSection.svelte
+++ src/lib/storyblok/HighlightSection.svelte
@@ -10,6 +10,8 @@
 
 	export let blok: any;
 
+	let videoPlayer!: HTMLVideoElement;
+
 	const textStyle = cva('duration-1000 ease-out', {
 		variants: {
 			theme: {
@@ -21,30 +23,50 @@
 			theme: 'light'
 		}
 	});
+
+	const inViewPlayer = ({ detail }: CustomEvent<ObserverEventDetails>) => {
+		const { inView } = detail as ObserverEventDetails;
+
+		if (inView) {
+			videoPlayer.play();
+		} else {
+			videoPlayer.pause();
+		}
+	};
 </script>
 
-<div
-	use:storyblokEditable={blok}
-	{...$$restProps}
-	use:inview
-	on:inview_change={inViewColorTransition}
-	class={cls('relative grid grid-cols-12', blok.class, textStyle({ theme: $backgroundTheme }))}
->
-	<div class="col-span-10 col-start-3">
-		<video class="aspect-video w-full" src={blok.video} autoplay={true} loop={true} muted={true}>
-			<track kind="captions" />
-		</video>
-	</div>
-	<div class=" col-span-10 col-start-2">
-		<div class=" max-w-[600px] -translate-y-28">
-			<Heading as="h2" size="h2"
-				>{blok.title}<span class="text-white">{blok.highlight}</span></Heading
+<div use:inview on:inview_change={inViewPlayer}>
+	<div
+		use:storyblokEditable={blok}
+		{...$$restProps}
+		use:inview
+		on:inview_change={inViewColorTransition}
+		class={cls('relative grid grid-cols-12', blok.class, textStyle({ theme: $backgroundTheme }))}
+	>
+		<div class="col-span-10 col-start-3">
+			<video
+				class="aspect-video w-full"
+				bind:this={videoPlayer}
+				src={blok.video}
+				autoplay={true}
+				loop={true}
+				muted={true}
 			>
-			<Spacer gap="extra-large" />
-			<a href={blok.link.cached_url}
-				><Heading size="h2" underline as="h4" class="text-rose-50 underline">{blok.label}</Heading
-				></a
-			>
+				<track kind="captions" />
+			</video>
+		</div>
+		<div class=" col-span-10 col-start-2">
+			<div class=" max-w-[600px] -translate-y-28">
+				<Heading as="h2" size="h2">
+					{blok.title}<span class="text-white">{blok.highlight}</span>
+				</Heading>
+				<Spacer gap="extra-large" />
+				<a href={blok.link.cached_url}>
+					<Heading size="h2" underline as="h4" class="text-rose-50 underline">
+						{blok.label}
+					</Heading>
+				</a>
+			</div>
 		</div>
 	</div>
 </div>
