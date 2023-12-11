8190b24ed10949532f167e821eec20152a0b5781
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-12-11 21:58:33 +0000

    Fix issue with video loader

--- src/routes/Loader.svelte
+++ src/routes/Loader.svelte
@@ -6,6 +6,8 @@
 	// avoid to reload the loader animation each time we update the page
 	export let skip: boolean = false;
 
+	let isReady: boolean = false;
+
 	export const hide = () => {
 		active = false;
 	};
@@ -15,6 +17,9 @@
 	let active: boolean = true;
 
 	function ready() {
+		if (isReady) return;
+
+		isReady = true;
 		dispatch('complete');
 	}
 
@@ -24,21 +29,28 @@
 			return;
 		}
 
-		logo.addEventListener('ended', ready);
-
-		return () => {
-			if (!skip) {
-				logo.removeEventListener('ended', ready);
-			}
-		};
+		checkTimeVideo();
 	});
+
+	function checkTimeVideo() {
+		if (logo.currentTime / logo.duration > 0.99) {
+			ready();
+		}
+	}
 </script>
 
 {#if active}
 	<div class="fixed left-0 top-0 z-40 h-screen w-screen overflow-hidden bg-white">
 		<div class="flex h-full w-full items-center justify-center">
 			<div class="absolute h-full w-full bg-white" />
-			<video bind:this={logo} playsinline autoplay muted class="absolute scale-50">
+			<video
+				bind:this={logo}
+				playsinline
+				autoplay
+				on:timeupdate={checkTimeVideo}
+				muted
+				class="absolute scale-50"
+			>
 				<source src={`${base}/assets/GFC-intro.mp4`} />
 			</video>
 		</div>
