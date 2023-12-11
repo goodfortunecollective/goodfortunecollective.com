98f9343f652e1fc39079553684d897ec1c6505f3
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2023-12-11 23:23:13 +0000

    add image option for SEO tags

--- src/lib/storyblok/SEO.svelte
+++ src/lib/storyblok/SEO.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	export let blok: any;
 
-	let { title, description } = blok;
+	let { title, description, image } = blok;
 </script>
 
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
+	{#if image.filename?.length > 0}
+		<meta property="og:image" content={image.filename} />
+	{/if}
 </svelte:head>
