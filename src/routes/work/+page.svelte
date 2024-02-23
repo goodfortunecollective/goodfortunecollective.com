e2698e099561ed54c6addf47f92b2a3d3d3879ba
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2024-02-23 02:16:46 +0000

    Fix work page layout on bigger screen

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -40,7 +40,7 @@
 		'col-span-10 col-start-1 md:col-span-6 md:col-start-2 md:-mt-[16.66%] z-1 text-left md:text-left 2xl:col-span-5 2xl:col-start-3',
 		'col-span-10 col-start-3 md:col-span-6 md:col-start-5 md:mt-[16.66%] z-2 text-left md:text-right 2xl:col-span-5 2xl:col-start-5',
 		'col-span-10 col-start-1 md:col-span-4 md:col-start-2 md:-mt-[4.166%] z-1 text-left md:text-left 2xl:col-span-5 2xl:col-start-3',
-		'col-span-10 col-start-3 md:col-span-4 md:col-start-7 md:mt-0 z-2 text-left md:text-right 2xl:col-span-5 2xl:col-start-7',
+		'col-span-10 col-start-3 md:col-span-4 md:col-start-7 md:mt-0 z-2 text-left md:text-right 2xl:col-span-5 2xl:col-start-6',
 		'col-span-10 col-start-1 md:col-span-7 md:col-start-2 md:-mt-0 z-1 text-left md:text-left 2xl:col-span-5 2xl:col-start-3'
 	];
 
