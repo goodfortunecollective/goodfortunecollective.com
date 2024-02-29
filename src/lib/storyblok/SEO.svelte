40b4b48ded83f983cd93b4bb72d2ec858a2672cd
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2024-02-29 15:37:12 +0000

    update SEO meta

--- src/lib/storyblok/SEO.svelte
+++ src/lib/storyblok/SEO.svelte
@@ -7,7 +7,22 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
+	<meta name="theme-color" content="#000000" />
 	{#if image?.filename?.length > 0}
 		<meta property="og:image" content={image.filename} />
+	{/if}
+
+	<!-- OG -->
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:site_name" content="Good Fortune Collective" />
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	{#if image?.filename?.length > 0}
+		<meta name="twitter:image" content={image.filename} />
 	{/if}
 </svelte:head>
