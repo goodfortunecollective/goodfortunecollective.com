9df052f4ba05590d3c60c9a11ecb186d32f805c7
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2024-05-31 00:02:11 +0100

    Update ArchiveList layout

--- src/routes/work/ArchiveList.svelte
+++ src/routes/work/ArchiveList.svelte
@@ -1,7 +1,5 @@
 <div class="grid grid-cols-12">
-	<div
-		class="col-span-12 col-start-1 mx-4 md:col-span-10 md:col-start-2 md:mx-0 2xl:col-span-8 2xl:col-start-3"
-	>
+	<div class="col-span-12 col-start-1 mx-4 md:col-span-10 md:col-start-2 md:mx-0">
 		<ul class="m-auto flex-col items-center">
 			<slot />
 		</ul>
