4df974b4a1fbfbae9d457682f853c556b3eba55f
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2025-12-03 00:36:02 +0000

    update tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+import tailwindcss from '@tailwindcss/vite';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
 	server: { https: true, host: true },
-	plugins: [mkcert(), sveltekit()],
+	plugins: [mkcert(), tailwindcss(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
