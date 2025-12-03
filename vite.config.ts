4ea9473402bd0f305a77c0d79b1c9cdc76e97cc4
Jean-Michel Saulnier <jmsaulnier.dev@gmail.com>
2025-12-03 16:52:26 +0000

    fix mkcert ci

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import { defineConfig } from 'vitest/config';
 import tailwindcss from '@tailwindcss/vite';
 import mkcert from 'vite-plugin-mkcert';
 
+const useMkcert = process.env.CI !== 'true';
+
+const plugins = [tailwindcss(), sveltekit()];
+
+if (useMkcert) {
+	plugins.unshift(mkcert());
+}
+
 export default defineConfig({
 	server: { host: true },
-	plugins: [mkcert(), tailwindcss(), sveltekit()],
+	plugins,
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
